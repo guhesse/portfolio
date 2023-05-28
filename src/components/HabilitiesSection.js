1f3bf84c5fdf33448a3c91ef0ca800fdee577014
guhesse <hesse.gustavo@gmail.com>
2023-05-28 15:02:00 -0300

    add lottie animations for rounded progress

--- src/components/HabilitiesSection.js
+++ src/components/HabilitiesSection.js
@@ -1,9 +1,29 @@
-import React, { useContext } from 'react';
+import React from 'react';
+import RoundProgress43 from '../animations/rounded-progress-43'
+import Lottie from 'lottie-react';
 
 function HabilitiesSection() {
     return (
         <div className='HabilitiesSection'>
-            <div className="habilities-section"></div>
+            <div className="habilities-section">
+                <div className="software-container">
+                    <h1 className="section-title">Softwares</h1>
+                    <div className="rounded-progresses">
+                    <Lottie className="progress-rounded" loop='false' animationData={RoundProgress43} />
+                    <Lottie className="progress-rounded" loop='false' animationData={RoundProgress43} />
+                    <Lottie className="progress-rounded" loop='false' animationData={RoundProgress43} />
+                    <Lottie className="progress-rounded" loop='false' animationData={RoundProgress43} />
+                    <Lottie className="progress-rounded" loop='false' animationData={RoundProgress43} />
+                    <Lottie className="progress-rounded" loop='false' animationData={RoundProgress43} />
+                    <Lottie className="progress-rounded" loop='false' animationData={RoundProgress43} />
+                    <Lottie className="progress-rounded" loop='false' animationData={RoundProgress43} />
+                    <Lottie className="progress-rounded" loop='false' animationData={RoundProgress43} />
+                    </div>
+                </div>
+                <div className="hability-container">
+                    <h1 className="section-title">Habilidades</h1>
+                </div>
+            </div>
         </div>
     );
 }
