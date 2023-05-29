904b1b192c10619254b5905328e50823c35f8bf1
guhesse <hesse.gustavo@gmail.com>
2023-05-28 21:26:36 -0300

    add scroll trigger on softwares

--- src/components/RoundedProgress.js
+++ src/components/RoundedProgress.js
@@ -1,16 +1,53 @@
-import React from 'react';
-import Lottie from 'lottie-react';
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Lottie from 'lottie-web';
 
 const RoundedProgress = (props) => {
-    const progress = props.progress;
-    const software = props.software;
+  const { progress, software } = props;
+  const containerRef = useRef(null);
+  const animationRef = useRef(null);
 
-    return (
-        <div className="progress-rounded-contain">
-            <Lottie trigger="hover"  className="rounded-progress-animation" loop='false' animationData={progress} />
-            <p className="software-text">{software}</p>    
-        </div>
-    );
-}
+  useEffect(() => {
+    // Inicialização do ScrollTrigger
+    gsap.registerPlugin(ScrollTrigger);
 
-export default RoundedProgress
+    // Configuração do ScrollTrigger
+    ScrollTrigger.create({
+      trigger: containerRef.current,
+      start: 'top 80%', // Iniciar animação quando o topo do elemento estiver a 80% da altura da janela
+      onEnter: () => {
+        // Iniciar a animação Lottie
+        animationRef.current.play();
+      },
+      onLeaveBack: () => {
+        // Retroceder a animação Lottie quando o elemento sair da tela
+        animationRef.current.goToAndStop(0);
+      },
+    });
+
+    // Carregar a animação Lottie
+    animationRef.current = Lottie.loadAnimation({
+      container: containerRef.current,
+      animationData: progress,
+      loop: false,
+      autoplay: false,
+      rendererSettings: {
+        className: "rounded-progress-animation"
+      }
+    });
+
+    // Remover o ScrollTrigger quando o componente for desmontado
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+    };
+  }, [progress]);
+
+  return (
+    <div className="progress-rounded-contain" ref={containerRef}>
+      <p className="software-text">{software}</p>
+    </div>
+  );
+};
+
+export default RoundedProgress;
