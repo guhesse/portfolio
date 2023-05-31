9feb489889f969dffec206893e24dd58dc653804
guhesse <hesse.gustavo@gmail.com>
2023-05-30 23:05:39 -0300

    add habilities progress animation and layout

--- src/components/HabilitiesSection.js
+++ src/components/HabilitiesSection.js
@@ -1,30 +1,48 @@
 import React from 'react';
 import RoundedProgress from './RoundedProgress';
-import ProgressLess from '../animations/rounded-progress-43'
-import ProgressHalf from '../animations/rounded-progress-71'
-import ProgressFull from '../animations/rounded-progress-90'
+import LinearProgress from './LinearProgress';
+import LinearSpecialProgress from './LinearSpecialProgress';
+import ProgressRoundedLess from '../animations/rounded-progress-43';
+import ProgressRoundedHalf from '../animations/rounded-progress-71';
+import ProgressRoundedFull from '../animations/rounded-progress-90';
+import ProgressLinear0 from '../animations/linear-progress-0';
+import ProgressLinear60 from '../animations/linear-progress-60';
+import ProgressLinear75 from '../animations/linear-progress-75';
+import ProgressLinear90 from '../animations/linear-progress-90';
+import ProgressLinear100 from '../animations/linear-progress-100';
+
 
 
 function HabilitiesSection() {
     return (
         <div className="habilities-section">
             <div className="software-container">
+
                 <h1 className="section-title">Softwares</h1>
                 <div className="rounded-progresses">
-                    <RoundedProgress progress={ProgressFull} software={'Ae'} />
-                    <RoundedProgress progress={ProgressFull} software={'Ps'} />
-                    <RoundedProgress progress={ProgressHalf} software={'Ai'} />
-                    <RoundedProgress progress={ProgressHalf} software={'Html'} />
-                    <RoundedProgress progress={ProgressHalf} software={'Css'} />
-                    <RoundedProgress progress={ProgressLess} software={'Js'} />
-                    <RoundedProgress progress={ProgressLess} software={'Dw'} />
-                    <RoundedProgress progress={ProgressLess} software={'In'} />
-                    <RoundedProgress progress={ProgressLess} software={'React'} />
+                    <RoundedProgress progress={ProgressRoundedFull} software={'Ae'} />
+                    <RoundedProgress progress={ProgressRoundedFull} software={'Ps'} />
+                    <RoundedProgress progress={ProgressRoundedHalf} software={'Ai'} />
+                    <RoundedProgress progress={ProgressRoundedHalf} software={'Html'} />
+                    <RoundedProgress progress={ProgressRoundedHalf} software={'Css'} />
+                    <RoundedProgress progress={ProgressRoundedLess} software={'Js'} />
+                    <RoundedProgress progress={ProgressRoundedLess} software={'Dw'} />
+                    <RoundedProgress progress={ProgressRoundedLess} software={'In'} />
+                    <RoundedProgress progress={ProgressRoundedLess} software={'React'} />
                 </div>
             </div>
+
             <div className="hability-container">
                 <h1 className="section-title">Habilidades</h1>
+                <div className="linear-progresses">
+                    <LinearProgress progress={ProgressLinear90} software={'Animação'} />
+                    <LinearProgress progress={ProgressLinear75} software={'Programação'} />
+                    <LinearProgress progress={ProgressLinear60} software={'Direção de arte'} />
+                    <LinearProgress progress={ProgressLinear75} software={'Edição de áudio e vídeo'} />
+                    <LinearSpecialProgress progress={ProgressLinear0} software={'Habilidade especial'} />
+                </div>
             </div>
+
         </div>
     );
 }
---
+++ src/components/LinearSpecialProgress.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useRef } from 'react';
+import ScrollMagic from 'scrollmagic';
+import { gsap } from 'gsap';
+import Lottie from 'lottie-web';
+
+
+const LinearProgress = (props) => {
+  const { progress, software } = props;
+  const containerRef = useRef(null);
+  const animationRef = useRef(null);
+  const controllerRef = useRef(null);
+
+  useEffect(() => {
+
+    // Inicialização do ScrollMagic
+    controllerRef.current = new ScrollMagic.Controller();
+
+    // Carregar a animação Lottie
+    animationRef.current = Lottie.loadAnimation({
+      container: containerRef.current,
+      animationData: progress,
+      loop: false,
+      autoplay: false,
+      renderer: 'svg', // Certifique-se de usar o renderizador correto
+      rendererSettings: {
+        className: "rounded-progress-animation"
+      }
+    });
+
+    // Configuração do ScrollMagic Scene
+    new ScrollMagic.Scene({
+      triggerElement: containerRef.current,
+      triggerHook: 0.8, // Iniciar animação quando o topo do elemento estiver a 80% da altura da janela
+      reverse: true, // Retroceder a animação quando o elemento sair da tela
+    })
+      .on('enter', () => {
+        // Iniciar a animação Lottie
+        animationRef.current.play();
+      })
+      .on('leave', () => {
+        // Parar a animação Lottie
+        animationRef.current.stop();
+      })
+      .addTo(controllerRef.current);
+
+    // Remover o ScrollMagic Controller quando o componente for desmontado
+    return () => {
+      controllerRef.current.destroy(true);
+    };
+  }, [progress]);
+
+  return (
+    <div className="linear-progress-contain" ref={containerRef}>
+      <p className="hability-text">{software}</p>
+      <a href='#'><p className='special-btn'>?</p></a>
+    </div>
+  );
+};
+
+
+export default LinearProgress;
