b55dfaaac732e7d20b7df653201c4ab1e677d75b
guhesse <hesse.gustavo@gmail.com>
2023-06-08 22:47:21 -0300

    fix right side of Services Section

--- src/pages/Home/view/ServicesSection/components/ServicesDescription/view/DescriptionBoxes.js
+++ src/pages/Home/view/ServicesSection/components/ServicesDescription/view/DescriptionBoxes.js
@@ -4,9 +4,9 @@ import '../styles/description-boxes.scss'
 function DescriptionBoxes() {
 
     const descriptionList = [
-        { title: 'Motion & Design', description: `Combinando movimento e design, apaixonado por contar histórias através de animações impactantes e designs inovadores. A tipografia, as cores e o layout são meus maiores aliados ao criar designs únicos.` },
-        { title: 'UI & UX', description: `Do layout ao código, minha abordagem de UI/UX integra design e experiência do usuário para criar produtos digitais impactantes e altamente funcionais.` },
-        { title: 'Programação', description: `Criação de landing pages e banners esteticamente agradáveis e altamente funcionais, aplicando meu conhecimento em HTML, CSS, JavaScript e animações para cativar o público.` }
+        { title: 'Motion & Design', description: 'Combinando movimento e design, apaixonado por contar histórias através de animações impactantes e designs inovadores. A tipografia, as cores e o layout são meus maiores aliados ao criar designs únicos.' },
+        { title: 'UI & UX', description: 'Do layout ao código, minha abordagem de UI/UX integra design e experiência do usuário para criar produtos digitais impactantes e altamente funcionais.' },
+        { title: 'Programação', description: 'Criação de landing pages e banners esteticamente agradáveis e altamente funcionais, aplicando meu conhecimento em HTML, CSS, JavaScript e animações para cativar o público.' }
     ];
 
     return (
